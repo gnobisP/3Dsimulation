5fd09b9a18cf09fcbacdd29919c9dcf0ef999f84
Arthur-Secundino <artsecundino@gmail.com>
2026-05-24 21:01:29 -0300

    Inseridos vídeos nos tapetes

--- controllers/chapa_controller.py
+++ controllers/chapa_controller.py
@@ -1,38 +1,36 @@
-from panda3d.core import CardMaker
+from panda3d.core import CardMaker, MovieTexture
 
 
-def create_image_chapa(loader, render, image_path, position, scale):
-    chapa_root = render.attachNewNode("image_chapa_root")
+def create_video_chapa(loader, render, video_path, position, scale):
+    chapa_root = render.attachNewNode("video_chapa_root")
     chapa_root.setPos(*position)
     chapa_root.setScale(scale)
 
     chapa = loader.loadModel("assets/chapa.bam")
     chapa.reparentTo(chapa_root)
-    chapa.setPos(0, 0, 0)
-    chapa.setScale(1.0)
+    chapa.setPos(0, 0, -1)
+    chapa.setScale(5.0)
 
-    # Carrega a imagem como textura
-    image_texture = loader.loadTexture(image_path)
-
-    if not image_texture:
-        print(f"Não foi possível carregar a imagem em {image_path}")
+    video_texture = MovieTexture("tv_screen")
+    if not video_texture.read(video_path):
+        print(f"Não foi possível carregar o vídeo em {video_path}")
         return chapa_root
 
-    # Obtém dimensões da imagem
-    image_width = max(1, image_texture.getXSize())
-    image_height = max(1, image_texture.getYSize())
-    aspect = image_width / image_height
+    video_texture.setLoop(True)
+    video_texture.play()
 
-    # Cria a "tela" do chapa
-    card_maker = CardMaker("chapa_screen_card")
+    video_width = max(1, video_texture.getVideoWidth())
+    video_height = max(1, video_texture.getVideoHeight())
+    aspect = video_width / video_height
+
+    card_maker = CardMaker("tv_screen_card")
+    card_maker.setUvRange(video_texture)
     card_maker.setFrame(-1.0, 1.0, -1.0 / aspect, 1.0 / aspect)
 
     screen = chapa_root.attachNewNode(card_maker.generate())
     screen.setPos(0, 0, 0)
-    screen.setHpr(0, 0, 0)
-    screen.setScale(1.0)
-
-    # Aplica a textura
-    screen.setTexture(image_texture, 1)
+    screen.setHpr(-90, -90, -90)
+    screen.setScale(6.8)
+    screen.setTexture(video_texture, 1)
 
     return chapa_root
